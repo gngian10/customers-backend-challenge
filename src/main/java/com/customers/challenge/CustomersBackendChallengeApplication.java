a1d619b7ed693cd517040c6c3986465d6cd5c98a
gian <189485256+gngian10@users.noreply.github.com>
2026-09-23 23:39:26 -0500

    initialize customers backend challenge

---
+++ src/main/java/com/customers/challenge/CustomersBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.customers.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customers/challenge/CustomersBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customers.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
